e0721f8c7479a2a4e14f97ebab15ff02277d46c4
Jaffar Aman <amanjaffar50@gmail.com>
2021-10-18 12:05:33 +0500

    Js first Class

---
+++ app.js
@@ -0,0 +1,73 @@
+// alert("Hello World")
+
+// var number = 100;
+
+// var number2 = 200 ;
+
+
+// alert('number');
+
+/////STRING variable/////
+
+
+
+// var name = "jaffar" ;
+
+// alert(name)
+
+
+// var name = "jaffar aman";
+// var age = 20;
+
+
+// alert(age)
+
+
+// var num1 = 10.25555
+// alert(num1)
+
+
+    /////opts//
+
+
+// var num1 = 10 ;
+// var num2 = 5;
+
+// var add = num1 + num2
+// var sub = num1 - num2
+// var mul = num1 * num2
+// var div = num1 / num2
+// var per = num1 % num2
+
+// alert(per)
+
+
+
+
+var name = "Jaffar"
+var num20 = 20
+
+alert(name + num20)
+
+
+var num100 = 10;
+var num200 = "20";
+
+alert(num100 - num200)
+
+
+var num1 = 100;
+
+var num2 = "200"
+
+
+// alert(num1  num2)
+
+
+
+
+
+
+// alert(add)
+// alert(sub)
+
